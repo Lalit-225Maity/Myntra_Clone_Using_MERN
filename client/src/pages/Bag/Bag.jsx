a3da7e5cce0ed011a3cd6be989d640aecd9a779c
LALIT MAITY <maitylalit89@gmail.com>
2026-04-20 02:10:24 +0530

    bag complete

--- client/src/pages/Bag/Bag.jsx
+++ client/src/pages/Bag/Bag.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useEffect } from 'react'
 import { Helmet } from 'react-helmet'
 import axios from 'axios'
+import { TiTick } from "react-icons/ti";
+import { MdCurrencyRupee } from "react-icons/md";
 import { useState } from 'react'
 import './Bag.css'
 const Bag = () => {
@@ -12,19 +14,19 @@ const Bag = () => {
         const response = await axios.get('/api/getbag');
         console.log(response.data.checkUser);
         setbags(response.data.checkUser);
-        
+
       } catch (error) {
 
         console.error('Error fetching bag:', error);
       }
     })();
   }, [])
-const RemoveItem=async(_id)=>{
-  const response=await axios.post('/api/removeitems',{_id});
-  setTimeout(() => {
-    window.location.reload();
-  }, 2000);
-}
+  const RemoveItem = async (_id) => {
+    const response = await axios.post('/api/removeitems', { _id });
+    setTimeout(() => {
+      window.location.reload();
+    }, 2000);
+  }
   return (
     <div className='bag'  >
       <Helmet>
@@ -34,11 +36,23 @@ const RemoveItem=async(_id)=>{
       <div className="bag-items-details">
 
         <div className="bag-items">
-          {bags.length>0 ? bags.map((i) => (
+          {bags.length > 0 ? bags.map((i) => (
             <div className="bag-product-coontainer"  >
-             <img src={i.image} alt="" />
-              <p>{i.product_brand}</p>
-              <button onClick={()=>{RemoveItem(i._id)}}>Remove</button>
+              <img src={i.image} alt="" />
+              <div className="product-details-section">
+                <h4>{i.product_brand}</h4>
+                <p>{i.product_name}</p>
+                <p>size:{i.size}</p>
+                <span className='price-section'>
+                  <MdCurrencyRupee className='icon_30' />
+                  <p>{i.product_price}</p>
+                </span>
+                <span className='delivery-time'>
+                  <TiTick className='icon_30'/>
+                  <p>Delivery by {new Date(i.delivery_date).toDateString()}</p>
+                </span>
+              </div>
+              <button onClick={()=>{RemoveItem(i._id)}} className='remove-button'>Remove</button>
             </div>
           )) : <div className="image-bag">
             <img src="/cart.png" alt="" />
